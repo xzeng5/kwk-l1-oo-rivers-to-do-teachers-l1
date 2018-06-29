93e95824899a243880dc643f04e0a08b384e59f2
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-29 17:20:25 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,32 @@
 # river.rb
+class River
+
+  attr_writer :length, :countries, :discharge, :name
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def length
+    @length
+  end
+
+  def countries
+    @countries
+  end
+
+  def discharge
+    @discharge
+  end
+
+  def flood
+    @discharge = @discharge * 1.3
+  end
+  
+
+  def dry_up
+    @discharge = @discharge *0.5
+  end
+
+end
